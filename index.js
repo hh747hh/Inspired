7deafbadbdbd4700fb308a8e336972e91864d5a6
Woo Yun <hh747hh@gmail.com>
2016-04-23 15:19:46 -0400

    installed angular

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require("express");
 var app = express();
 
+app.use("/", express.static("public"));
+app.use("/", express.static("bower_components"));
+
+
 app.get("/", function(req,res){
   res.sendFile(__dirname + "/public/index.html");
 });
