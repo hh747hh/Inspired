f40c694865674b4813f095477400b598c7354c60
Woo Yun <hh747hh@gmail.com>
2016-04-24 06:33:32 -0400

    Added basic angular controllers

--- public/js/app.js
+++ public/js/app.js
@@ -5,20 +5,41 @@
   .module("breatheNow", [
     "ui.router"
   ])
-  .config(Router);
+  .config(Router)
+  .controller("teachersIndexController", teachersIndexCtrl)
+  .controller("teachersShowController", teachersShowCtrl);
 
   Router.$inject = ["$stateProvider", "$locationProvider", "$urlRouterProvider"];
   function Router($stateProvider, $locationProvider, $urlRouterProvider){
     $locationProvider.html5Mode(true);
     $stateProvider
-    .state("main", {
-      url:   "/",
-      template: "<h2>This is working!</h2>"
+    .state("teachersIndex", {
+      url:           "/",
+      templateUrl:   "/html/teachers-index.html",
+      controller:    "teachersIndexController",
+      controllerAs:  "teachersIndexVM"
     })
-    .state("test", {
-      url:   "/test",
-      template: "<h2>This also works</h2>"
+    .state("teachersShow", {
+      url:   "/teachers/:name",
+      templateUrl: "/html/teachers-show.html",
+      controller:    "teachersShowController",
+      controllerAs: "teachersShowVM"
     });
     $urlRouterProvider.otherwise("/");
   }
+
+  function teachersIndexCtrl(){
+    var vm = this;
+    vm.teachers = [
+    {name : "Dalai Lama"},
+    {name : "Tick Nat Hahn"},
+    {name : "Tara Brach"}
+  ];
+}
+  teachersShowCtrl.$inject = ["$stateParams"];
+  function teachersShowCtrl($stateParams){
+      var vm = this;
+      vm.teacher = $stateParams;
+  }
+
 })();
