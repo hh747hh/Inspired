12771279d37d593f6a05a2afb19df5919cec33de
Woo Yun <hh747hh@gmail.com>
2016-04-25 15:03:40 -0400

    trouble adding new teacher

--- index.js
+++ index.js
@@ -12,34 +12,36 @@ app.use("/", express.static("bower_components"));
 
 
 app.get("/api/teachers", function(req, res){
-  Teacher.find().then(function(teachers){
+  Teacher.find({}).then(function(teachers){
     res.json(teachers);
   });
 });
 
+app.post("/api/teachers", function(req, res){
+  console.log(req.body);
+  Teacher.create(req.body.teacher).then(function(teacher){
+    res.json(teacher);
+  });
+});
+
 app.get("/api/teachers/:name", function(req, res){
-  Teacher.findOne(req.params).then(function(teacher){
+  Teacher.findOne({name: req.params.name}).then(function(teacher){
     res.json(teacher);
   });
 });
 
 app.delete("/api/teachers/:name", function(req, res){
-  Teacher.findOneAndRemove(req.params).then(function(){
+  Teacher.findOneAndRemove({name: req.params.name}).then(function(){
     res.json({success: true});
   });
 });
 
 app.patch("/api/teachers/:name", function(req, res){
-  Teacher.findOneAndUpdate(req.params, req.body, {new: true}).then(function(Teacher){
+  Teacher.findOneAndUpdate({name: req.params.name}, req.body.teacher, {new: true}).then(function(teacher){
     res.json(teacher);
   });
 });
 
-app.post("/api/teachers", function(req, res){
-  Teacher.create(req.body).then(function(teacher){
-    res.json(teacher);
-  });
-});
 
 app.get("/*", function(req,res){
   res.sendFile(__dirname + "/public/index.html");
