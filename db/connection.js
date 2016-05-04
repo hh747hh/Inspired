02be630819061b43f597c133f3db4e4ab64b6d12
Woo Yun <hh747hh@gmail.com>
2016-05-04 10:59:22 -0400

    change connection to mongodb_url

--- db/connection.js
+++ db/connection.js
@@ -22,7 +22,7 @@ var UserSchema = {
 
 
 if(process.env.NODE_ENV == "production"){
-  mongoose.connect(process.env.MONGODB_URI); // make sure this says MONGODB NOT MONGOLAB
+  mongoose.connect(process.env.MONGODB_URL); // make sure this says MONGODB NOT MONGOLAB
 }else{
   mongoose.connect("mongodb://localhost/breatheNow");
 } //heroku attempt
